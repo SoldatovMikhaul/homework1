248020e56d61cc6ba703b5a5983b01691e1d829b
Солдатов Михаил Андреевич <msoldatov@phoenixit.ru>
2022-10-05 18:33:59 +0300

    Add constatnts to year value

--- src/test/java/com/homework/homework1/PredictTest.java
+++ src/test/java/com/homework/homework1/PredictTest.java
@@ -9,11 +9,14 @@ import static com.homework.homework1.Predict.predictValueForTomorrow;
 import static org.junit.Assert.assertEquals;
 
 public class PredictTest {
+    private static final int YEAR = 2000;
+    private static final int MONTH = 12;
+    private static final int DAY_OF_MONTH = 11;
     @Test
     public void shouldCorrectPredictValueForNextDay_whenSimpleDataGivenInCorrectInput() {
         Float[] data = new Float[]{1.11f, 1.15f, 1.14f, 1.13f, 1.14f, 1.15f, 1.23f};
         String expected = "MO 11.12.346 - 1.15";
-        assertEquals(expected,  predictValueForTomorrow(data, LocalDate.of(2000, 12, 11)));
+        assertEquals(expected,  predictValueForTomorrow(data, LocalDate.of(YEAR, MONTH, DAY_OF_MONTH)));
     }
 
     @Test
@@ -26,6 +29,6 @@ public class PredictTest {
                 "SA 16.12.351 - 1.1630194\n" +
                 "SU 17.12.352 - 1.1663082\n" +
                 "MO 18.12.353 - 1.1686379\n";
-        assertEquals(expected,  predictValueForNextWeek(data, LocalDate.of(2000, 12, 11)));
+        assertEquals(expected,  predictValueForNextWeek(data, LocalDate.of(YEAR, MONTH, DAY_OF_MONTH)));
     }
 }
